dbcc25a13aefcd4224e06867ac969e7ae09eb0dd
Chori <yidodeveloper@gmail.com>
2023-12-21 01:51:28 +0900

    first commit

---
+++ src/main/java/com/miracle/miraclemorningback/MiracleMorningBackApplication.java
@@ -0,0 +1,13 @@
+package com.miracle.miraclemorningback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracleMorningBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiracleMorningBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miracle/miraclemorningback/MiracleMorningBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miracle.miraclemorningback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleMorningBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
