ce0ea48bfd9824be1d39014deae86d0402796052
Chori <yidodeveloper@gmail.com>
2024-01-27 22:06:48 +0900

    20240127 gitignore 수정, 불필요한 프론트엔드 파일 주석 처리

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,18 +1,18 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
-const password = document.getElementById("password");
-const generatePasswordBtn = document.getElementById("generatePassword");
+// const signUpButton = document.getElementById("signUp");
+// const signInButton = document.getElementById("signIn");
+// const container = document.getElementById("container");
+// const password = document.getElementById("password");
+// const generatePasswordBtn = document.getElementById("generatePassword");
 
-signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
+// signUpButton.addEventListener("click", () => {
+//   container.classList.add("right-panel-active");
+// });
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener("click", () => {
+//   container.classList.remove("right-panel-active");
+// });
 
-generatePasswordBtn.addEventListener("click", () => {
-  const randomStr = Math.random().toString(36).substring(2, 8);
-  password.value = randomStr;
-});
+// generatePasswordBtn.addEventListener("click", () => {
+//   const randomStr = Math.random().toString(36).substring(2, 8);
+//   password.value = randomStr;
+// });
