7d7af0f6761fc1c9e918730774a6adbdb6aadacf
Chori <yidodeveloper@gmail.com>
2024-02-21 00:00:26 +0900

    20240221 CORS 정책에서 허용하는 출처 설정

--- src/main/java/com/miracle/miraclemorningback/security/CorsConfig.java
+++ src/main/java/com/miracle/miraclemorningback/security/CorsConfig.java
@@ -2,20 +2,27 @@ package com.miracle.miraclemorningback.security;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+// 개발 환경에서 CORS 에러 방지
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
+
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of(allowedOrigins));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
